df41a45a3961b6683e69ca518689edebfa8322a9
hasanabill <hasanabilna@gmail.com>
2022-05-24 21:10:36 +0600

    post to cart added

--- index.js
+++ index.js
@@ -7,6 +7,44 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8pzbh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect()
+        const toolsCollection = client.db('shounen-manu').collection('tools')
+        const cartCollection = client.db('shounen-manu').collection('cart')
+
+
+        app.get('/tools', async (req, res) => {
+            const result = await toolsCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/tool/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await toolsCollection.findOne(query)
+            res.send(result);
+        })
+
+        app.post('/cart', async (req, res) => {
+            const cartItem = req.body;
+            const result = await cartCollection.insertOne(cartItem)
+            res.send(result)
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
+
 app.get('/', (req, res) => {
     res.send('Server in up')
 })
