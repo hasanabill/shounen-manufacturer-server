7b80290b1b92eff4b2ac0bb67e29b03acb388ec5
hasanabill <hasanabilna@gmail.com>
2022-05-25 10:48:51 +0600

    user and jwt added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,12 +13,40 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8pzbh.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next()
+    });
+}
+
 async function run() {
     try {
         await client.connect()
         const toolsCollection = client.db('shounen-manu').collection('tools')
         const cartCollection = client.db('shounen-manu').collection('cart')
+        const userCollection = client.db('shounen-manu').collection('users')
 
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email }
+            const options = { upsert: true }
+            const updatedDoc = {
+                $set: user
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc, options)
+            const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1d' })
+            res.send({ result, token });
+        })
 
         app.get('/tools', async (req, res) => {
             const result = await toolsCollection.find().toArray()
@@ -37,7 +66,7 @@ async function run() {
             res.send(result)
         })
 
-        app.get('/cart/:email', async (req, res) => {
+        app.get('/cart/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
             const query = { email }
             const result = await cartCollection.find(query).toArray()
