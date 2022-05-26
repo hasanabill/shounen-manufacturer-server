08fa15d618f4e432980ebfbc9db5af7453f92d9b
hasanabill <hasanabilna@gmail.com>
2022-05-26 22:11:28 +0600

    verifyAdmin added

--- index.js
+++ index.js
@@ -37,6 +37,17 @@ async function run() {
         const userCollection = client.db('shounen-manu').collection('users')
         const reviewCollection = client.db('shounen-manu').collection('reviews')
 
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester })
+            if (requesterAccount.role === 'admin') {
+                next()
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' })
+            }
+        }
+
         // sending user to database
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
@@ -206,7 +217,7 @@ async function run() {
         })
 
         // giving admin role
-        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
             const email = req.params.email;
             const requester = req?.decoded?.email;
             const requesterAcc = await userCollection.findOne({ email: requester })
